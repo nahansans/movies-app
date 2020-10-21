453e5cfe058623799beede7606d9987a57aa3cc3
Naufal Hanif <nahanifg@gmail.com>
2020-10-21 14:16:43 +0700

    load movies by menu

--- src/references/API.tsx
+++ src/references/API.tsx
@@ -1,5 +1,7 @@
 export const BASE_URL = 'https://api.themoviedb.org/3'
 
-export const API_KEY = '?api_key=7b40e41ba0b87090d54aa558e4eda5fa&language=en-US'
+const YOUR_API_KEY = "" // taruh api key nya disini
+
+export const API_KEY = `?api_key=${YOUR_API_KEY}&language=en-US`
 export const BACKDROP_URL = 'https://image.tmdb.org/t/p/w370_and_h556_multi_faces/'
 export const POSTER_URL = 'https://image.tmdb.org/t/p/w370_and_h556_multi_faces'
